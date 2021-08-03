03d1871a2374453284ce1535420295ad2b1ee13c
cxtch <iain.sh.li@gmail.com>
2021-08-03 02:55:21 -0400

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const WebSocket = require('ws')
+const config = require('./config.json')
+const osu = new WebSocket(`ws://localhost:${config.port}/ws`)
+const scopes = ['rpc', 'rpc.activities.write'];
+const DiscordRichPresence = require('discord-rpc')
+const client = new DiscordRichPresence.Client({
+  'transport': 'ipc'
+})
+client.login({
+  clientId: config.client_id,
+  scopes: scopes,
+  redirectUri: 'https://github.com/cxtch/gosu-rich-presence',
+  clientSecret: config.client_secret,
+})
+client.on('ready', () => {
+  console.log(`sucessfully connected to ${client.user.username}`)
+})
+osu.on('message', (incoming) => {
+  let data = JSON.parse(incoming)
+  let state = '';
+  if (data.menu.state == 1)
+    state = 'In the editor';
+  if (data.menu.state == 2);
+  state = `Clicking circles [${data.menu.mods.str}]`
+  client.setActivity({
+    details: `${data.menu.bm.metadata.artist} - ${data.menu.bm.metadata.title} | ${data.menu.bm.metadata.mapper}`,
+    state: `${state}`,
+    buttons: [{
+        label: 'beatmap',
+        url: `https://osu.ppy.sh/b/${data.menu.bm.id}`
+      },
+      {
+        label: 'profile',
+        url: `https://osu.ppy.sh/u/${config.profile}`
+      }
+    ]
+  })
+})
