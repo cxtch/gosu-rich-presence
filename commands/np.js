f3c27b683c2001283fdbcd094e37c378f60f71e4
cxtch <iain.sh.li@gmail.com>
2021-08-27 20:50:49 -0400

    fix np

--- commands/np.js
+++ commands/np.js
@@ -2,12 +2,16 @@ module.exports = {
   description: "Copy the current beatmap to the clipboard",
   run(_this) {
     const clipboardy = require('clipboardy');
-    let beatmap = `https://osu.ppy.sh/b/${_this.cache.menu.bm.id}`
+    let beatmap;
+    if (_this.cache.menu.bm.id)
+      beatmap = `https://osu.ppy.sh/b/${_this.cache.menu.bm.id}`;
+    else
+      beatmap = `https://osu.ppy.sh/beatmapsets/${_this.cache.menu.bm.set}`;
     try {
-      clipboardy.writeSync(beatmap)
-      console.log('beatmap copied to clipboard', beatmap)
+      clipboardy.writeSync(beatmap);
+      console.log('beatmap copied to clipboard', beatmap);
     } catch (err) {
-      console.log(beatmap)
+      console.log(beatmap);
     }
   }
 }
